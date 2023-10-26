0e76fbb23b86f149c720f11470f570a2e3e09ca8
RicoEmbrado <embradorico@gmail.com>
2023-10-26 12:57:56 -0700

    I added the screens

---
+++ app/src/main/java/com/example/marktakanoproject/solutionsScreen.java
@@ -0,0 +1,14 @@
+package com.example.marktakanoproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class solutionsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solutions_screen);
+    }
+}
