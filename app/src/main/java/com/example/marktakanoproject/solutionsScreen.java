1133605909e68635cabc3666163c939a24fb423d
RicoEmbrado <embradorico@gmail.com>
2023-10-30 13:23:01 -0700

    Added dropdown box and button and textview

--- app/src/main/java/com/example/marktakanoproject/solutionsScreen.java
+++ app/src/main/java/com/example/marktakanoproject/solutionsScreen.java
@@ -3,12 +3,29 @@ package com.example.marktakanoproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class solutionsScreen extends AppCompatActivity {
 
+    Button confirmButton = findViewById(R.id.confirmButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solutions_screen);
-    }
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stringArray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                String selectedItem = spinner.getSelectedItem().toString();
+            }
+        }
+    };
 }
