aa0439534459012739987b06e3fcc21564a82ca2
Rico Embrado <embradorico@gmail.com>
2023-10-31 18:29:55 -0700

    Finished off solutions part 2.

--- app/src/main/java/com/example/marktakanoproject/solutionsScreen.java
+++ app/src/main/java/com/example/marktakanoproject/solutionsScreen.java
@@ -22,6 +22,7 @@ public class solutionsScreen extends AppCompatActivity
         Button confirmButton = findViewById(R.id.confirmButton);
         Spinner spinner = findViewById(R.id.spinner);
         TextView textViewOutput = findViewById(R.id.textViewOutput);
+        TextView textViewOutput2 = findViewById(R.id.textViewOutput2);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.stringArray, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -42,22 +43,27 @@ public class solutionsScreen extends AppCompatActivity
                 else if (selectedItem.equals("Moreno Valley"))
                 {
                     textViewOutput.setText("Moreno Valley Family Health Center\nMoreno Valley Community Health Center\nUnicare Community Health Center - Moreno Valley");
+                    textViewOutput2.setText("Julio Hernandez Rodriguez, MD\nAsia Sullivan, PA-C, MPH\nGregory Cason, PhD");
                 }//end of else if statement
                 else if (selectedItem.equals("Riverside"))
                 {
                     textViewOutput.setText("Kaiser Permanente\nPacific Grove Hospital\nParkview Community");
+                    textViewOutput2.setText("Andrea Seldomridge, PsyD\nFelipe Cendejas, DMSc, PA-C\nRebecca Williams, LMFT");
                 }//end of else if statement
                 else if (selectedItem.equals("Fontana"))
                 {
                     textViewOutput.setText("Unicare Community Health Center - Fontana\nUrgent Care - Dignity Health - Fontana, CA\nUnicare Community Health Center - Fontana");
+                    textViewOutput2.setText("Michelle McNevin, LCSW\nDeAndrea Knight, LCSW\nLila Goldsman Phd\n");
                 }//end of else if statement
                 else if (selectedItem.equals("Corona"))
                 {
                     textViewOutput.setText("Corona Community Health Center\nMEDICROSS CLINIC & URGENT CARE\nCentro Medico Community Clinic");
+                    textViewOutput2.setText("Florence Yuan, MD\nLindsey Ritter-Ingorvaia, AMFT, APCC\nJudith Morgan, LE");
                 }//end of else if statement
                 else if (selectedItem.equals("San Bernardino"))
                 {
                     textViewOutput.setText("Adelanto Health Center\nHesperia Health Center\nOntario Health Center");
+                    textViewOutput2.setText("Kevin Chun, MD\nFrank R. Patti, LMFT, PsyD\nGene Park, DDS, MS");
                 }//end of else if statement
             }//end of onClick method
         });//end of onclick event handler
